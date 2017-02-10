2c5f44b858dedda2355c94aba354026a40dae345
Kyle Novak <kylenovak@github.com>
2017-02-09 21:50:56 -0600

    fix text caps

--- functions/filters/filter-post-entry-meta.php
+++ functions/filters/filter-post-entry-meta.php
@@ -3,9 +3,9 @@
 // Filter the post entry default before text.
 function post_meta_filter($post_meta) {
   if ( 'book_review' == get_post_type() ) {//swap in CPT name
-		$post_meta='[post_terms taxonomy="genre" before="Posted Under: "]';//swap in taxonomy and label name
+		$post_meta='[post_terms taxonomy="genre" before="Posted under: "]';//swap in taxonomy and label name
 	} elseif ( 'post' == get_post_type() ) {
-    $post_meta = '[post_categories before="Posted Under: "] [post_tags before="Tagged With: "]';
+    $post_meta = '[post_categories before="Posted under: "] [post_tags before="Tagged with: "]';
   }
   return $post_meta;
 }
